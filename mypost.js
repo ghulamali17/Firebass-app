bb7bbf2ceeb007fecdd2e23711b3a5931192054e
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2025-02-24 13:37:56 +0500

    Create mypost.js

---
+++ mypost.js
@@ -0,0 +1,41 @@
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import { db, auth } from "./firebaseConfig.js";
+
+let myPostDiv = document.querySelector("#myPosts");
+
+let loggedInUser = localStorage.getItem("loggedInUser");
+if (!loggedInUser) {
+  window.location.replace("./index.html");
+}
+
+let getMyPosts = async () => {
+  try {
+    myPostDiv.innerHTML = "";
+    const q = query(collection(db, "posts"), where("uid", "==", loggedInUser));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((post) => {
+      myPostDiv.innerHTML += `<div class="post-box">${post.data().postText}</div>`;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+document.querySelector("#signOut").addEventListener("click", async () => {
+  await signOut(auth)
+    .then(() => {
+      localStorage.removeItem("loggedInUser");
+      window.location.replace("./index.html");
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+});
+
+getMyPosts();

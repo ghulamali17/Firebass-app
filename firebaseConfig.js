8dbdbf4e5b81e1f4d63d33b1da7b0df962ff60c1
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2025-02-23 23:30:20 -0800

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,4 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";

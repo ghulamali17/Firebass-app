db470c0f37f53bea91df7cbed4942fba90e063c5
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2025-02-23 23:46:36 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,71 +1,83 @@
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-import { auth, db } from "./firebaseConfig.js";
-import {
-  addDoc,
-  collection,
-  query,
-  where,
-  getDocs,
-  limit,
-  orderBy,
-  Timestamp,
-  doc,
-  getDoc,
-} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+import { createUserWithEmailAndPassword ,signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import { auth , db} from "./firebaseConfig.js";
+import { addDoc,collection } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
-// let createPost = async () => {
-//   try {
-//     let date =  new Date();
-//    // Add a new document in collection "cities"
-// await addDoc(collection(db, "posts"), {
-//   text:'this is the 1st post',
-//   createdAt: date.getTime(),
-// });
+let addUserData = async(user)=>{
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    email: user.email, 
+    phoneNumber: user.phoneNumber, 
+    photoURL: user.photoURL, 
+    displayName: user.displayName, 
+    uid: user.uid,
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+}
 
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-// createPost();
-
-
-let data = []
-
-let getRequest = async () => {
-  try {
-    // Create a query against the collection.
-    const q = query(
-      collection(db, "f_request"),
-      where("to", "==", 'o4xvzUFpX9gfM9JT8o4eREFs4PB3'),
-    );
-    let querySnapshot = await getDocs(q);
-    // console.log(querySnapshot);
-    if (querySnapshot.docs.length < 1) {
-      console.log("no data found");
-      return;
-    }
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-      data.push({id:doc.id, ...doc.data()})
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
-getRequest().then(()=>{
-  data.forEach(async()=>{
-    try{
-      let user = await getDoc(doc(db,'users', 'o4xvzUFpX9gfM9JT8o4eREFs4PB3'))
-      console.log(user)
-     }catch(e){
-      console.error(e)
-    }
+let signUpUser = async(email,password)=>{
+  
+await createUserWithEmailAndPassword(auth, email, password)
+.then((userCredential) => {
+  // Signed up 
+  const user = userCredential.user;
+  
+  console.log('successful login')
+  console.log(user)
+  addUserData(user).then(()=>{
+    localStorage.setItem('loggedInUser', user.uid)
+    window.location.replace('./dashboard.html');
   })
+  
+
+  // ...
+})
+.catch((error) => {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  console.error(errorCode,errorMessage)
+  // ..
+});
+}
+
+
+document.querySelector('#signUp-btn').addEventListener('click',()=>{
+  var emailValue = document.querySelector('#email').value;
+  var passwordValue = document.querySelector('#password').value;
+  // console.log(emailValue,passwordValue);
+  signUpUser(emailValue,passwordValue);
+
+
+
 })
 
 
 
+let signIn = async (email,pass) => {
+  await signInWithEmailAndPassword(auth, email,pass)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log("login success: ", user);
+      // ...
+        localStorage.setItem('loggedInUser', user.uid)
+        window.location.replace('./dashboard.html');
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(errorCode, errorMessage);
+    });
+};
+// signIn()
+
+document.querySelector('#login-btn').addEventListener('click',()=>{
+  var emailValue = document.querySelector('#email').value;
+  var passwordValue = document.querySelector('#password').value;
+  // console.log(emailValue,passwordValue);
+signIn(emailValue,passwordValue);
 
 
-
-
+})

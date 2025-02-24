f63cf5cb3ff740ce5470ab92cebf35dbcee8958c
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2025-02-24 16:29:19 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,6 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 
+import { signInWithEmailAndPassword } from 
     "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-import { auth, db } from "./firebaseConfig.js";
-import { addDoc, collection } from 
-    "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
-
-let addUserData = async (user) => {
-    try {
-        await addDoc(collection(db, "users"), {
-            email: user.email,
-            uid: user.uid,
-        });
-    } catch (e) {
-        alert("Error adding user data: " + e.message);
-        console.error("Error adding document: ", e);
-    }
-};
-
-let signUpUser = async (email, password) => {
-    try {
-        if (!email || !password) {
-            alert("Email and Password cannot be empty!");
-            return;
-        }
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-        await addUserData(user);
-        localStorage.setItem("loggedInUser", user.uid);
-        alert("Sign Up Successful!");
-        window.location.replace("./dashboard.html");
-    } catch (error) {
-        alert("Sign Up Error: " + error.message);
-        console.error(error.code, error.message);
-    }
-};
+import { auth } from "./firebaseConfig.js";
 
 let signIn = async (email, password) => {
     try {
@@ -42,8 +10,8 @@ let signIn = async (email, password) => {
         }
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
         const user = userCredential.user;
-        localStorage.setItem("loggedInUser", user.uid);
         alert("Login Successful!");
+        localStorage.setItem("loggedInUser", user.uid);
         window.location.replace("./dashboard.html");
     } catch (error) {
         alert("Login Error: " + error.message);
@@ -51,14 +19,10 @@ let signIn = async (email, password) => {
     }
 };
 
-document.querySelector("#signUp-btn").addEventListener("click", () => {
-    let email = document.querySelector("#email").value;
-    let password = document.querySelector("#password").value;
-    signUpUser(email, password);
-});
-
-document.querySelector("#login-btn").addEventListener("click", () => {
-    let email = document.querySelector("#email").value;
-    let password = document.querySelector("#password").value;
-    signIn(email, password);
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#login-btn").addEventListener("click", () => {
+        let email = document.querySelector("#email").value.trim();
+        let password = document.querySelector("#password").value.trim();
+        signIn(email, password);
+    });
 });

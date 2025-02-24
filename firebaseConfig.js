0b92cd002e69ca0b4bbab2930265b58c694628e6
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2025-02-24 12:31:24 +0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,3 +2,19 @@
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
   import { getAuth } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
   import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+  // Import the functions you need from the SDKs you need
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCZpZWIJ0mLgOqAzMC5jMbBdOgAgn_rL2M",
+    authDomain: "ecommerce-19ff4.firebaseapp.com",
+    projectId: "ecommerce-19ff4",
+    storageBucket: "ecommerce-19ff4.firebasestorage.app",
+    messagingSenderId: "521272247200",
+    appId: "1:521272247200:web:7996007c1a00fe39ecdac5"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
